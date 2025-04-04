1fca85164a9064787cc7c22e4515f9089b80e21a
Yurii Fito <fito.pn@ucu.edu.ua>
2025-04-04 11:37:11 +0300

    Linked Lists - Remove Duplicates

---
+++ middle/linked_lists_remove_duplicates/linked_lists_remove_duplicates.py
@@ -0,0 +1,15 @@
+class Node(object):
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+def remove_duplicates(head):
+    if head is None:
+        return head
+    current = head
+    while current.next:
+        if current.data == current.next.data:
+            current.next = current.next.next
+        else:
+            current = current.next
+    return head
