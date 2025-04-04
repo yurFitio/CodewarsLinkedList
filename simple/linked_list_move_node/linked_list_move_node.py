ab54ddc5681c3776a328e68a8a4a638fd08f50b4
Yurii Fito <fito.pn@ucu.edu.ua>
2025-04-04 11:30:29 +0300

    Linked list - Move Node

---
+++ simple/linked_list_move_node/linked_list_move_node.py
@@ -0,0 +1,14 @@
+class Node(object):
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+    
+class Context(object):
+    def __init__(self, source, dest):
+        self.source = source
+        self.dest = dest
+    
+def move_node(source, dest):
+    source, front_node = source.next, source
+    front_node.next = dest
+    return Context(source, front_node)
